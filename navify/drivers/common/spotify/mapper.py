1162d808829794dd903c08cb75c3960544d5ae22
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2024-12-17 16:20:19 +0100

    bug fixes

--- navify/drivers/common/spotify/mapper.py
+++ navify/drivers/common/spotify/mapper.py
@@ -4,12 +4,15 @@ from navify.models import Playlist, Track
 class SpotifyMapper(ServiceMapper):
     """Maps Spotify API DTOs to internal models."""
 
-    def map_playlist(self, data: dict) -> 'Playlist':        
-        service_id = data.get('id')
-        name = data.get('name')
-        description = data.get('description')
+    def map_playlist(self, data: dict) -> 'Playlist':  
+        if isinstance(data, type(None)):
+            raise ValueError('Input data cannot be None')
+              
+        service_id = data.get('id', None)
+        name = data.get('name', None)
+        description = data.get('description', None)
         is_public = data.get('public', False)
-        author_name = data.get('owner').get('display_name')
+        author_name = data.get('owner', {}).get('display_name', None)
         
         return Playlist(
             service_id=service_id,
@@ -22,15 +25,23 @@ class SpotifyMapper(ServiceMapper):
         )
     
     def map_track(self, data: dict) -> 'Track':
-        service_id = data.get('id')
-        title = data.get('name')
-        album_name = data.get('album').get('name')
-        primary_artist = data.get('artists')[0].get('name')
-        additional_artists = [artist.get('name') for artist in data.get('artists')[1:]]
-        duration_seconds = int(data.get('duration_ms') / 1000) if data.get('duration_ms') else None
-        track_number = int(data.get('track_number')) if data.get('track_number') else None
-        release_year = int(data.get('album').get('release_date')[:4]) if data.get('album').get('release_date') else None
-        isrc = data.get('external_ids').get('isrc') if data.get('external_ids') else None
+        if isinstance(data, type(None)):
+            raise ValueError('Input data cannot be None')
+        
+        _raw_artists = data.get('artists', [])
+        
+        service_id = data.get('id', None)
+        title = data.get('name', None)
+        album_name = data.get('album', {}).get('name', None)
+        primary_artist = _raw_artists[0].get('name', None) if len(_raw_artists) > 0 else None
+        duration_seconds = int(data.get('duration_ms', None) / 1000) if data.get('duration_ms', None) else None
+        track_number = int(data.get('track_number', None)) if data.get('track_number', None) else None
+        release_year = int(data.get('album', {}).get('release_date', None)[:4]) if data.get('album', {}).get('release_date', None) else None
+        isrc = data.get('external_ids', {}).get('isrc', None) if data.get('external_ids', None) else None
+        
+        additional_artists = []
+        if len(_raw_artists) > 1:
+            additional_artists = [artist.get('name', None) for artist in _raw_artists[1:]]
         
         return Track(
             title=title,
