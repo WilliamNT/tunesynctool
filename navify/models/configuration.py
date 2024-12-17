36b8369a7919e2f34dba4ed799a772ed3a1ac0c8
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2024-12-17 13:40:01 +0100

    added documentation to code

--- navify/models/configuration.py
+++ navify/models/configuration.py
@@ -7,17 +7,67 @@ class Configuration:
     """Single source of truth for configuration parameters."""
 
     spotify_client_id: str = field(default=None)
+    """
+    Spotify client ID. Required for Spotify API access.
+
+    Learn more: https://developer.spotify.com/documentation/web-api/concepts/apps
+    """
+
     spotify_client_secret: str = field(default=None)
+    """
+    Spotify client secret. Required for Spotify API access.
+
+    Learn more: https://developer.spotify.com/documentation/web-api/concepts/apps
+    """
+
     spotify_redirect_uri: str = field(default='http://localhost:8888/callback')
+    """
+    Spotify redirect URI. Required for Spotify API access.
+
+    Learn more: https://developer.spotify.com/documentation/web-api/concepts/apps
+    """
+
     spotify_scopes: str = field(default='user-library-read,playlist-read-private,playlist-read-collaborative,playlist-modify-public,playlist-modify-private')
+    """
+    A list of Spotify scopes, seperated with commas. Required for Spotify API access. You can override this, but shouldn't unless you know what you're doing.
+    If changed, make sure to reauthenticate.
+
+    Learn more: https://developer.spotify.com/documentation/web-api/concepts/scopes
+    """
     
     whitelisted_ids: List[str] = field(default_factory=list)
+    """
+    A list of Spotify playlist IDs, seperated with commas. Only playlists with these IDs will be processed. If not set, all playlists will be processed.
+
+    Learn more: https://developer.spotify.com/documentation/web-api/concepts/spotify-uris-ids
+    """
+
     preview_only: bool = field(default=True)
+    """
+    Toggles preview mode. If set to True, no changes will be made to the target service, but all features will run like usual. If set to False, changes will be made.
+    """
 
     subsonic_base_url: str = field(default="http://127.0.0.1")
+    """
+    Base URL for the Subsonic server. Required for Subsonic API access.
+    Should include the protocol (http/https) and the domain or IP address.
+    A valid value for example would be "https://demo.subsonic.org" or "http://192.168.1.2".
+    """
+
     subsonic_port: int = field(default=4533)
+    """
+    Port for the Subsonic server. Required for Subsonic API access.
+    """
+
     subsonic_username: str = field(default=None)
+    """
+    Username for the Subsonic server. Required for Subsonic API access.
+    """
+
     subsonic_password: str = field(default=None)
+    """
+    Password for the Subsonic server. Required for Subsonic API access.
+    """
 
     @classmethod
     def from_env(cls) -> 'Configuration':
