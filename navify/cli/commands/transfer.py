0bce992738bf227c5f7d2f2501a30b29d24e2111
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2025-01-05 22:33:06 +0100

    add preview option to playlist transfer command

--- navify/cli/commands/transfer.py
+++ navify/cli/commands/transfer.py
@@ -11,8 +11,9 @@ from tqdm import tqdm
 @pass_obj
 @option('--from', 'from_provider', type=Choice(SUPPORTED_PROVIDERS), required=True, help='The provider to copy the playlist from.')
 @option('--to', 'to_provider', type=Choice(SUPPORTED_PROVIDERS), required=True, help='The target provider to copy the playlist to.')
+@option('--preview', 'is_preview', is_flag=True, show_default=True, default=False, help='Preview the transfer without actually touching the target service.')
 @argument('playlist_id', type=str, required=True)
-def transfer(ctx: Optional[dict], from_provider: str, to_provider: str, playlist_id: str):
+def transfer(ctx: Optional[dict], from_provider: str, to_provider: str, playlist_id: str, is_preview: bool):
     """Transfers a playlist from one provider to another."""
 
     try:
@@ -27,7 +28,6 @@ def transfer(ctx: Optional[dict], from_provider: str, to_provider: str, playlist
     echo(style(f"Found playlist: {source_playlist}", fg='green'))
 
     source_tracks = source_driver.get_playlist_tracks(source_playlist.service_id)
-    echo(style(f"Found {len(source_tracks)} tracks in the playlist"))
 
     matcher = TrackMatcher(target_driver)
     matched_tracks = []
@@ -41,11 +41,17 @@ def transfer(ctx: Optional[dict], from_provider: str, to_provider: str, playlist
         else:
             tqdm.write(style(f"No match found for \"{track}\"", fg='yellow'))
 
+    echo(style(f"Found {len(matched_tracks)} matches in total", fg='green'))
+
+    if is_preview:
+        echo(style("Preview transfer complete", fg='green'))
+        return
+    
     target_playlist = target_driver.create_playlist(source_playlist.name)
 
     target_driver.add_tracks_to_playlist(
         playlist_id=target_playlist.service_id,
         track_ids=[track.service_id for track in matched_tracks],
     )
-    echo(style(f"Added {len(matched_tracks)} tracks to the playlist", fg='green'))
+    
     echo(style(f"Playlist transfer complete!", fg='green'))
