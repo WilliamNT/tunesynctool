2e128e612152ccb9045c60257ac13a56b55f0ae8
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2025-01-12 16:34:01 +0100

    added doc comments for excheptions

--- tunesynctool/exceptions/__init__.py
+++ tunesynctool/exceptions/__init__.py
@@ -1,12 +1,18 @@
 class PlaylistNotFoundException(Exception):
+    """Indicates that a playlist could not be found or retrieved for technical reasons from the streaming service."""
+    
     def __init__(self, message="Playlist not found."):
         super().__init__(message)
 
 class TrackNotFoundException(Exception):
+    """Indicates that a track could not be found or retrieved for technical reasons from the streaming service."""
+    
     def __init__(self, message="Track not found."):
         super().__init__(message)
 
 class ServiceDriverException(Exception):
+    """Should be raised when an error occurs in the driver that is not related to the streaming service or no better exception exists."""
+    
     def __init__(self, message="Unknown driver error."):
         super().__init__(message)
 
