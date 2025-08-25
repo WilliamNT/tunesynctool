b438b9ee590377f3eac18757ac6c7908deb4e570
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2025-08-25 12:24:50 +0200

    fallback message to improve ux

--- webui/frontend/src/views/HomeView.vue
+++ webui/frontend/src/views/HomeView.vue
@@ -8,6 +8,7 @@ import { get_authenticated_api_configuration } from '@/services/api';
 import { computed, onMounted, ref } from 'vue';
 import { useUser } from '@/composables/useUser';
 import AppLoaderScreen from '@/components/generic/AppLoaderScreen.vue';
+import { Icon } from '@iconify/vue';
 
 const providers = ref<ProviderRead[]>([]);
 const playlists = ref<PlaylistRead[]>([]);
@@ -31,10 +32,13 @@ onMounted(async () => {
   isLoading.value = false;
 });
 
+const areAnyProvidersLinked = computed(() => {
+  return providers.value.filter(p => p.linking.linked).length > 0
+});
+
 const getPlaylistKey = (playlist: PlaylistRead) => {
   return `${playlist.meta.provider_name}-${playlist.identifiers.provider_id}`;
 };
-
 
 const providersWithPlaylists = computed(() => {
   return providers.value.filter((provider) => {
@@ -55,16 +59,24 @@ const providersWithPlaylists = computed(() => {
           Here are your playlists from all linked services.
         </template>
       </AppPageHeader>
-      <div class="flex flex-col gap-4 items-center mt-10" v-for="provider in providersWithPlaylists" :key="provider.provider_name">
-        <div class="flex items-center gap-3 w-full mb-3">
-          <img :src="provider.ui.favicon" alt="" class="w-6 h-6" />
-          <h2 class="text-2xl">{{ provider.ui.display_name }}</h2>
-          <hr class="flex-1 border-zinc-700 border-0.5 ms-5" />
+      <template v-if="providersWithPlaylists.length > 0">
+        <div class="flex flex-col gap-4 items-center mt-10" v-for="provider in providersWithPlaylists" :key="provider.provider_name">
+          <div class="flex items-center gap-3 w-full mb-3">
+            <img :src="provider.ui.favicon" alt="" class="w-6 h-6" />
+            <h2 class="text-2xl">{{ provider.ui.display_name }}</h2>
+            <hr class="flex-1 border-zinc-700 border-0.5 ms-5" />
+          </div>
+          <div class="flex gap-4 flex-wrap">
+            <template v-for="playlist in playlists" :key="getPlaylistKey(playlist)">
+              <PlaylistCard :providers :playlist v-if="provider.provider_name === playlist.meta.provider_name" />
+            </template>
+          </div>
         </div>
-        <div class="flex gap-4 flex-wrap">
-          <template v-for="playlist in playlists" :key="getPlaylistKey(playlist)">
-            <PlaylistCard :providers :playlist v-if="provider.provider_name === playlist.meta.provider_name" />
-          </template>
+      </template>
+      <div class="flex items-center justify-center h-full" v-else>
+        <div class="flex flex-col items-center gap-4">
+          <Icon icon="material-symbols-light:library-music-rounded" class="text-gray-400 text-7xl"/>
+          <p class="text-md font-normal text-gray-400">{{ areAnyProvidersLinked ? 'Your library is empty.' : 'Please configure at least one provider to access your library.' }}</p>
         </div>
       </div>
     </template>
